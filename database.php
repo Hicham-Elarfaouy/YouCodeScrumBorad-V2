25d29a2106a4608abb84cc2ded52322ce4888d24
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2022-11-01 10:24:40 +0100

    implement dotenv

--- database.php
+++ database.php
@@ -1,8 +1,12 @@
 <?php
+    require 'vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     function connection(){
         //CONNECT TO MYSQL DATABASE USING MYSQLI
-        $link = mysqli_connect("localhost", "root", "", "youcodescumboard");
+        $link = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 
         // Check connection
         if($link === false){
